3e72328893a46432afd582e12c7b5b473a07ca68
JunjiYoshizuka999 <yabbeame999@gmail.com>
2023-03-02 16:52:53 +0900

    商品一覧表示機能修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :situation
-  belongs_to :shippingcharge
+  belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :numberofday
 
