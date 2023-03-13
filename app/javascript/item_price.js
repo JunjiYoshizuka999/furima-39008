9f25e14ca24a55e7ea147185624e797555c1227c
JunjiYoshizuka999 <yabbeame999@gmail.com>
2023-03-13 11:35:14 +0900

    本番環境修正１

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => {
     const addTaxDom = document.getElementById('add-tax-price');
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     const profitDom = document.getElementById('profit');
-    profitDom.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
   })
 
 });
